b5658791efe0614bbce0d70b1bb6305ce2ae3385
fernando flores <martinjanns2000@gmail.com>
2022-12-29 15:09:50 -0600

    bhfhbh

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import { default as routerSendEmail } from './routers/sendEmail.router.js';
 import { PUERTO } from './env/configEnv.js';
-import path from 'path'
 
 const app = express();
 
-// función middleware para servir archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
+
 // Configuramos cors para evitar problemas de peticiones a los endpoints
 
 app.use(cors())
